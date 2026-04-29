55badfc3b59cada2a3ee9650a8708a567d62eedd
nicole-2022 <nicole.sanchezc@campusucc.edu.co>
2026-04-29 15:06:30 +0000

    avance

---
+++ TraInd/DeshacerPila.java
@@ -0,0 +1,12 @@
+package TraInd;
+
+import java.util.Stack;
+
+public class DeshacerPila {
+
+    public static void main(String[] args) {
+        
+        Stack<String> acciones = new Stack<>()
+    }
+    
+}
