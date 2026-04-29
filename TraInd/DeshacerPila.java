b51d504a38a43ec68cfadc91cb0a421e7d4bf04a
nicole-2022 <nicole.sanchezc@campusucc.edu.co>
2026-04-29 17:54:53 +0000

    avance

--- TraInd/DeshacerPila.java
+++ TraInd/DeshacerPila.java
@@ -1,12 +1,36 @@
-package TraInd;
-
+import java.util.Scanner;
 import java.util.Stack;
 
 public class DeshacerPila {
 
     public static void main(String[] args) {
-        
-        Stack<Character> verParentesis = new Stack<>()
+        Scanner usu = new Scanner(System.in);
+        Stack<Character> pila = new Stack<>();
+
+        System.out.println("Dime una expresión con paréntesis:");
+        String texto = usu.nextLine();
+
+        for (int i = 0; i < texto.length(); i++) {
+            char c = texto.charAt(i);
+
+            if (c == '(') {
+                pila.push(c);
+            } else if (c == ')') {
+                if (pila.isEmpty()) {
+                    System.out.println("Error: hay un ) sin abrir");
+                    return;
+                } else {
+                    pila.pop();
+                }
+            }
+        }
+
+        if (pila.isEmpty()) {
+            System.out.println("Paréntesis correctos ");
+        } else {
+            System.out.println("Error: faltan cerrar paréntesis ");
+        }
+
+        usu.close();
     }
-    
 }
